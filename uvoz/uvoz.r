dafb36f7e8f1f89dd9955c5c4a7e3de07a2b9fdd
Maruša Valant <valantmarusa@gmail.com>
2014-11-27 16:39:44 +0100

    Dodajanje tortnega grafa

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -22,12 +22,31 @@
 uvoziSLO <- function() {
   return(read.table("podatki/BRUTOSLO.csv", sep = ";", as.is = TRUE,
                   
-        
-                  
-                    
+                   
+                    col.names = c("Leto", "", "Dejavnosti", "SLOVENIJA", "Zahodna Slovenija", "Obalno-kraška", "Goriška", "Gorenjska", "Osrednjeslovenska",	"Vzhodna Slovenija", "Notranjsko-kraška", "Jugovzhodna Slovenija", "Spodnjeposavska", "Zasavska",	"Savinjska", "Koroška", "Podravska",	"Pomurska"
+),                  
+                   
+                   
                     fileEncoding = "Windows-1250"))
 }
 
 #Zapišimo podatke v razpredelnico druzine.
 cat("Uvažam podatke o BRUTOSLO...\n")
 BRUTOSLO <- uvoziSLO()
+
+
+slices <- c(2.7, 25.2, 20.8, 5.9, 20.4, 4.3, 4.5, 7.5, 9.1, 17.8, 2.7)
+lbls <- c("A Kmetijstvo, gozdarstvo in ribištvo",
+          "BCDE Predelovalne dejavnosti", "rudarstvo in druga industrija", "Predelovalne dejavnosti",
+          "F Gradbeništvo", "GHI Trgovina", "gostinstvo", "promet", "J Informacijske in komunikacijske dejavnosti",
+          "K Finančne in zavarovalniške dejavnosti", "L Poslovanje z nepremičninami",
+          "MN Strokovne, znanstvene, tehnične in druge posl. dejavnosti",
+          "OPQ Uprava in obramba, obv. soc. varnost, izob., zdravstvo",
+          "RSTU Druge dejavnosti")
+pct <- round(slices/sum(slices)*100)
+lbls <- paste(lbls, pct) # add percents to labels 
+lbls <- paste(lbls,"%",sep="") # ad % to labels 
+pie(slices,labels = lbls, col=rainbow(length(lbls)),
+    main="Struktura po dejavnostih v Sloveniji leta 2012")
+
+
